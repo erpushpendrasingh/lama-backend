3c6e16e148c343d249b17a5e4f5fff1aeeeddf05
Pushpendra Singh <erpushpendrasingh8900@gmail.com>
2023-01-29 21:02:36 +0530

    strip route

---
+++ routes/strip.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const strip = require("stripe")(process.env.STRIPE_KEY);
+
+router.post("/payment", (req, res) => {
+     strip.charges.create(
+          {
+               source: req.body.tokenId,
+               amount: req.body.amount,
+               currency: "usd",
+          },
+          (stripErr, stripeRes) => {
+               if (stripErr) {
+                    res.status(500).json(stripErr);
+               } else {
+                    res.status(200).json(stripeRes);
+               }
+          }
+     );
+});
+
+module.exports = router;
